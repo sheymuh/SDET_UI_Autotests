c40a4998a4fc244769dd3c7038d2b60a47b06f5f
ANDREI_SHEIMUKHOV <sheymuhov05@mail.ru>
2026-06-08 20:29:58 +0400

    add DataProvider in LoginPageTests

--- src/test/java/com/simbirsoft/pages/LoginPage.java
+++ src/test/java/com/simbirsoft/pages/LoginPage.java
@@ -1,6 +1,7 @@
 package com.simbirsoft.pages;
 
 import io.qameta.allure.Step;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -20,6 +21,10 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class LoginPage extends BasePage {
     public static final String LOGIN_SUCCESS_MESSAGE = "You're logged in!!";
     public static final String LOGIN_FAILURE_MESSAGE = "Username or password is incorrect";
+    public static final String USERNAME_EMPTY_MESSAGE = "You did not enter a username";
+    public static final String PASSWORD_EMPTY_MESSAGE = "You did not enter a password";
+    public static final String USERNAME_SHORT_MESSAGE = "Your username must be between 3 and 50 characters long";
+    public static final String PASSWORD_SHORT_MESSAGE = "Your password must be between 3 and 100 characters long";
 
     @FindBy(id = "username")
     private WebElement usernameField;
@@ -48,6 +53,14 @@ public class LoginPage extends BasePage {
     @FindBy(css = "[href='#/login']")
     private WebElement logoutButton;
 
+    // Сообщение валидации поля username
+    @FindBy(xpath = "//div[@ng-messages='form.username.$error']//div[@class='ng-scope']")
+    private WebElement usernameValidationMessage;
+
+    // Сообщение валидации поля password
+    @FindBy(xpath = "//div[@ng-messages='form.password.$error']//div[@class='ng-scope']")
+    private WebElement passwordValidationMessage;
+
     public LoginPage(WebDriver driver, WebDriverWait waiter) {
         super(driver, waiter);
     }
@@ -62,6 +75,24 @@ public class LoginPage extends BasePage {
         return incorrectLoginDataAlert;
     }
 
+    @Step("Получение текста сообщения валидации поля 'Username'")
+    public String getUsernameValidationMessageText() {
+        try {
+            return usernameValidationMessage.getText();
+        } catch (NoSuchElementException ex) {
+            return null;
+        }
+    }
+
+    @Step("Получение текста сообщения валидации поля 'Password'")
+    public String getPasswordValidationMessageText() {
+        try {
+            return passwordValidationMessage.getText();
+        } catch (NoSuchElementException ex) {
+            return null;
+        }
+    }
+
     @Step("Проверка доступности кнопки 'Login'")
     public boolean isLoginButtonEnabled() {
         return loginButton.isEnabled();
@@ -70,6 +101,11 @@ public class LoginPage extends BasePage {
     @Step("Проверка отображения всех полей формы")
     public boolean allFieldsAreDisplayed() {
         return usernameField.isDisplayed() && passwordField.isDisplayed() && usernameDescriptionField.isDisplayed();
+    }
+
+    @Step("Проверка все ли поля формы пусты")
+    public boolean allFieldsAreEmpty() {
+        return usernameField.getText().isEmpty() && passwordField.getText().isEmpty() && usernameDescriptionField.getText().isEmpty();
     }
 
     @Step("Проверка все ли поля заполнены")
