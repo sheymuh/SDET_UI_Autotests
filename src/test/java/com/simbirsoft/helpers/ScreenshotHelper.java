65e0dfdb6f577ef7f72b76710aec8b6c47a7a1c7
ANDREI_SHEIMUKHOV <sheymuhov05@mail.ru>
2026-06-09 12:26:03 +0400

    add takeScreenshot in test tearDown

---
+++ src/test/java/com/simbirsoft/helpers/ScreenshotHelper.java
@@ -0,0 +1,47 @@
+package com.simbirsoft.helpers;
+
+import io.qameta.allure.Attachment;
+import org.openqa.selenium.WebDriver;
+import ru.yandex.qatools.ashot.AShot;
+import ru.yandex.qatools.ashot.Screenshot;
+
+import static ru.yandex.qatools.ashot.util.ImageTool.toByteArray;
+
+public class ScreenshotHelper {
+    /**
+     * Скриншот страницы через aShot
+     */
+    @Attachment(value = "aShot screenshot", type = "image/png")
+    public static byte[] takeAShotScreenshot(WebDriver driver) {
+        try {
+            Screenshot screenshot = new AShot()
+                    .takeScreenshot(driver);
+            return toByteArray(screenshot.getImage());
+        } catch (Exception ex) {
+            attachScreenshotError("Failed to take aShot screenshot: " + ex.getMessage());
+            return new byte[0];
+        }
+    }
+
+    /**
+     * Базовый скриншот через Selenium
+     */
+    @Attachment(value = "Screenshot", type = "image/png")
+    public static byte[] takeScreenshot(WebDriver driver) {
+        try {
+            return ((org.openqa.selenium.TakesScreenshot) driver)
+                    .getScreenshotAs(org.openqa.selenium.OutputType.BYTES);
+        } catch (Exception e) {
+            attachScreenshotError("Failed to take screenshot: " + e.getMessage());
+            return new byte[0];
+        }
+    }
+
+    /**
+     * Фиксация ошибки скриншота в Allure-отчёте
+     */
+    @Attachment(value = "Screenshot Error", type = "text/plain")
+    public static String attachScreenshotError(String errorMessage) {
+        return errorMessage;
+    }
+}
