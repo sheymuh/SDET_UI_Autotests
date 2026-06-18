23e35cd5ae0f19388e69e45e6fb69f19d7ba9800
ANDREI_SHEIMUKHOV <sheymuhov05@mail.ru>
2026-06-18 19:32:06 +0400

    add tabs test

---
+++ src/test/java/com/simbirsoft/pages/FramesAndWindowsPage.java
@@ -0,0 +1,37 @@
+package com.simbirsoft.pages;
+
+import io.qameta.allure.Step;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class FramesAndWindowsPage extends BasePage {
+    @FindBy(css = "div#example-1-tab-1 iframe.demo-frame")
+    private WebElement demoFrame;
+
+    @FindBy(xpath = "//a[contains(text(),'New Browser Tab')]")
+    private WebElement newTabLink;
+
+    public FramesAndWindowsPage(WebDriver driver, WebDriverWait waiter) {
+        super(driver, waiter);
+    }
+
+    public WebDriver switchToNewWindow(WebDriver driver) {
+        Object[] windowHandles = driver.getWindowHandles().toArray();
+        return driver.switchTo().window((String) windowHandles[windowHandles.length - 1]);
+    }
+
+    @Step("Ожидание доступности фрейма demoFrame и фокус на нем")
+    public void waitForDemoFrame() {
+        waiter.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(demoFrame));
+    }
+
+    @Step("Переход по ссылке 'New Tab Link'")
+    public FramesAndWindowsPage clickNewTabLink() {
+        newTabLink.click();
+
+        return this;
+    }
+}
